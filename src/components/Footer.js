7dc23bf641a8bd4aa96987e0a57e84d5191a6462
josanjohnata <josanjohnata@gmail.com>
2021-08-11 17:54:52 -0300

    Corrige o Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,33 +2,31 @@ import React from 'react';
 
 function Footer() {
   return (
-    <h1>esse é o footer</h1>
-//       <footer className="footer_area">
-// 		<div class="container">
-// 			<div class="row justify-content-center">
-// 				<div class="col-lg-12">
-// 					<div class="footer_top flex-column">
-// 						<div class="footer_logo">
-// 							<div href="./">
-// 								<img src="img/logo.png" alt="">
-// 							</div>
-// 							<h4>Siga me</h4>
-// 						</div>
-// 						<div class="footer_social">
-// 							<a target="_blank" href="https://www.linkedin.com/in/douglas-he/"><i class="fa fa-linkedin"></i></a>
-// 							<a target="_blank" href="https://github.com/douglas-he"><i class="fa fa-github"></i></a>
-// 							<a target="_blank" href="https://www.instagram.com/doug.he/"><i class="fa fa-instagram"></i></a>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</div>
-// 			<div class="row footer_bottom justify-content-center">
-// 				<p class="col-lg-8 col-sm-12 footer-text">
-			
-// Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a</p>
-// 			</div>
-// 		</div>
-// 	</footer>
-  )}
+		<div>
+			<div>
+				<div>
+					<div>
+						<div>
+							<div>
+								<img src="./" alt="" />
+							</div>
+							<h4>Siga me</h4>
+						</div>
+						<div>
+							<a href="https://www.linkedin.com/in/josanjohnata/"><i class="fa fa-linkedin"></i></a>
+							<a href="https://github.com/josanjohnata"><i class="fa fa-github"></i></a>
+							<a href="https://www.instagram.com/johnatapontes/"><i class="fa fa-instagram"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<p>			
+Copyright &copy;2021 All rights reserved | Johnata Pontes
+</p>
+			</div>
+		</div>
+  );
+}
 
 export default Footer;
