c58c196f6f18acbcacbcf035d9a03301434411b9
josanjohnata <josanjohnata@gmail.com>
2021-08-11 22:29:55 -0300

    Adiciona a pagina Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Header />
-    <Footer />
+    <Home />
     </>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+function Home() {
+  return (
+    <dev>
+      <Header />
+      <section>
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <h3>Olá</h3>
+                  <h1>Eu sou o Johnata Pontes</h1>
+                  <h5>Desenvolvedor Junior FullStack</h5>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <img src="./" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </dev>
+  );
+}
+
+export default Home;
